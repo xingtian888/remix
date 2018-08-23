f7e42d99bf7f6a20efa19e3af0620a795e7a8bca
Iuri Matias <iuri.matias@gmail.com>
2018-08-23 10:28:31 -0400

    add version & help options

--- remix-tests/src/run.js
+++ remix-tests/src/run.js
@@ -5,6 +5,18 @@ const fs = require('fs')
 
 const Provider = require('remix-simulator').Provider
 
+const version = require('../package.json').version
+
+commander.version(version)
+
+commander.command('version').description('output the version number').action(function () {
+  console.log(version)
+});
+
+commander.command('help').description('output usage information').action(function () {
+  commander.help()
+});
+
 commander.action(function (filename) {
   let web3 = new Web3()
   // web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
